4e4dd849fb53eec2d29d83c45d09cef165e7cf87
mlerc-jv <jimenavirmeru@gmail.com>
2025-07-02 17:08:21 -0300

    EJERCICIOO 3

--- tp6_ldobles/ejer3.cpp
+++ tp6_ldobles/ejer3.cpp
@@ -53,32 +53,7 @@ void AgregarFinal(tlista &lista, pnodo nuevo){
 		lista.fin = nuevo;
 	}
 }
-	
-	void QuitarInicio(tlista &lista) {
-		if (lista.inicio != NULL ) {
-			pnodo dele = lista.inicio;
-			if (lista.inicio == lista.fin) { 
-				lista.inicio = lista.fin = NULL;
-			} else {
-				lista.inicio = lista.inicio -> sig;
-				lista.inicio -> ant = NULL;
-			}
-			delete dele;
-		}
-	}
-	
-	void QuitarFinal(tlista &lista) {
-		if (lista.fin != NULL) {
-			pnodo dele = lista.fin;
-			if (lista.inicio == lista.fin) { 
-				lista.inicio = lista.fin = NULL;
-			} else {
-				lista.fin = lista.fin -> ant;
-				lista.fin -> sig = NULL;
-			}
-			delete dele;
-		}
-	}
+
 	void DecimalABinario(tlista &lista, int numero) {
 		pnodo nuevo;
 		if (numero == 0) {
